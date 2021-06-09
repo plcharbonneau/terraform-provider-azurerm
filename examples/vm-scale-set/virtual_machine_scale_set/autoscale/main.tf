8fa385016254b1c8c1cef176813add79e6787c1d
Pier-Luc Charbonneau <charbonneaupl@gmail.com>
2021-06-09 08:32:17 -0400

    Update main.tf

--- examples/vm-scale-set/virtual_machine_scale_set/autoscale/main.tf
+++ examples/vm-scale-set/virtual_machine_scale_set/autoscale/main.tf
@@ -1,9 +1,4 @@
 provider "azurerm" {
-  subscription_id = var.ARM_SUBSCRIPTION
-  client_id       = var.ARM_CLIENT_ID
-  client_secret   = var.ARM_CLIENT_SECRET
-  tenant_id       = var.ARM_TENANT_ID
-
   features {}
 }
 
